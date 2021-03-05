f58bd837671eabdaebdd90f044f82c7725aa9e28
Dora Budić <dbudic0706@gmail.com>
2021-03-05 15:09:43 +0100

    First condition modified

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 for (var i = 1; i < 101; i++) {
 
   // controllare se sono multipli di 3 o 5 o 3 e 5
-  if (!(i%3) && !(i%5)) {
-    document.getElementById('list').innerHTML += "<li>" + "FizzBuzz" + "</li>";
+  if (!(i%15)) { //alternativa (!(i%3) && !(i%5))
+    document.getElementById('container').innerHTML += "<p>" + "FizzBuzz" + "</p>";
   } else if (!(i%3)) {
-    document.getElementById('list').innerHTML += "<li>" + "Fizz" + "</li>";
+    document.getElementById('container').innerHTML += "<p>" + "Fizz" + "</p>";
   } else if (!(i%5)) {
-    document.getElementById('list').innerHTML += "<li>" + "Buzz" + "</li>";
+    document.getElementById('container').innerHTML += "<p>" + "Buzz" + "</p>";
   } else {
-    document.getElementById('list').innerHTML += "<li>" + i + "</li>";
+    document.getElementById('container').innerHTML += "<p>" + i + "</p>";
   }
 }
