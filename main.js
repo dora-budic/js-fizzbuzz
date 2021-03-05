e6bffb9a20be664f27b91e28fa88e0fa8e2cb571
Dora Budić <dbudic0706@gmail.com>
2021-03-05 17:52:09 +0100

    Added events in js

--- main.js
+++ main.js
@@ -12,3 +12,57 @@ for (var i = 1; i < 101; i++) {
     document.getElementById('container').innerHTML += "<p>" + i + "</p>";
   }
 }
+
+var listTitle = document.getElementById('title');
+console.log(listTitle);
+listTitle.addEventListener('click', function(e) {
+  e.target.style.fontStyle = "italic";
+});
+
+
+var elements = document.getElementById('container').children;
+
+for (var i = 0; i < elements.length-2; i++) {
+  i = i+2;
+  elements[i].style.color = "#FF7E05";
+  elements[i].style.fontWeight = "bold";
+  elements[i].addEventListener('mouseover', function(e) {
+    e.target.style.color = "#fff";
+  });
+  elements[i].addEventListener('mouseout', function(e) {
+    e.target.style.color = "#FF7E05";
+  });
+  elements[i].addEventListener('click', function(e) {
+    e.target.style.fontSize = "20px";
+  });
+}
+
+for (var i = 0; i < elements.length-4; i++) {
+  i = i+4;
+  elements[i].style.color = "#F710D4";
+  elements[i].style.fontWeight = "bold";
+  elements[i].addEventListener('mouseover', function(e) {
+    e.target.style.color = "#fff";
+  });
+  elements[i].addEventListener('mouseout', function(e) {
+    e.target.style.color = "#F710D4";
+  });
+  elements[i].addEventListener('click', function(e) {
+    e.target.style.fontSize = "20px";
+  });
+}
+
+for (var i = 0; i < elements.length-14; i++) {
+  i = i+14;
+  elements[i].style.color = "#F7F710";
+  elements[i].style.fontWeight = "bold";
+  elements[i].addEventListener('mouseover', function(e) {
+    e.target.style.color = "#fff";
+  });
+  elements[i].addEventListener('mouseout', function(e) {
+    e.target.style.color = "#F7F710";
+  });
+  elements[i].addEventListener('click', function(e) {
+    e.target.style.fontSize = "20px";
+  });
+}
